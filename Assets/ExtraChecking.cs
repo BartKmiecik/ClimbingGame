b2970db28871fd9acd6217be0629bbf6127e8986
kmiecikJob <pracakmiecik@gmail.com>
2021-07-28 12:03:58 +0200

    Designate angles values for fall checking

---
+++ Assets/ExtraChecking.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// In case if only 2 or 1 limbs touching handlers, count weight middle point and designate angles to check if player should fall. If only 2 legs touching handlers you can move body weight in 90 degree 
+// vertical radius from point between foots, when only hand and leg from same side(left or right) touching handler you have 60degre horizontal, in case of only 1 leg or hand 15 degree;
+public class ExtraChecking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/DisplayMIddlePoint.cs
+++ Assets/Scripts/DisplayMIddlePoint.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class DisplayMIddlePoint : MonoBehaviour
 {
     [SerializeField] private Transform p1, p2, p3, p4;
+    [SerializeField] private LimbsCollisionChecker limbsCollisionChecker;
     private void OnDrawGizmos()
     {
-        float x1 = (p1.position.x + p3.position.x) / 2;
-        float y1 = (p1.position.y + p3.position.y) / 2;
-        float z1 = (p1.position.z + p3.position.z) / 2;
+        float x1 = (p1.position.x + p2.position.x) / 2;
+        float y1 = (p1.position.y + p2.position.y) / 2;
+        float z1 = (p1.position.z + p2.position.z) / 2;
 
-        float x2 = (p2.position.x + p4.position.x) / 2;
-        float y2 = (p2.position.y + p4.position.y) / 2;
-        float z2 = (p2.position.z + p4.position.z) / 2;
+        float x2 = (p3.position.x + p4.position.x) / 2;
+        float y2 = (p3.position.y + p4.position.y) / 2;
+        float z2 = (p3.position.z + p4.position.z) / 2;
 
-        float finalX = (x1 + x2)/ 2;
+        float finalX = (x1 + x2) / 2;
         float finalY = (y1 + y2) / 2;
         float finalZ = (z1 + z2) / 2;
 
@@ -23,11 +24,67 @@ public class DisplayMIddlePoint : MonoBehaviour
 
         Gizmos.color = Color.red;
 
-        Gizmos.DrawSphere(final, .2f);
+        Gizmos.DrawSphere(final, .05f);
+
 
         Gizmos.color = Color.green;
 
-        Gizmos.DrawLine(p4.position, p4.position + new Vector3(1f, 3.81f,0));
-        Gizmos.DrawLine(p4.position, p4.position + new Vector3(-1f, 3.81f, 0));
+        float angler = 0;
+
+        if (limbsCollisionChecker.leftFoot && limbsCollisionChecker.rightFoot)
+        {
+            Vector3 temp = new Vector3(x2, y2, z2);
+            Gizmos.DrawLine(temp, temp + new Vector3(1f, .6f, 0));
+            Gizmos.DrawLine(temp, temp + new Vector3(-1f, .6f, 0));
+            angler = Mathf.Atan2(y2 - finalY, x2 - finalX) * 180 / Mathf.PI;
+        }
+        else if (limbsCollisionChecker.rightFoot && limbsCollisionChecker.rightHand)
+        {
+            Vector3 temp = new Vector3((p1.position.x + p3.position.x) / 2, (p1.position.y + p3.position.y) / 2, (p1.position.z + p3.position.z) / 2);
+            Gizmos.DrawLine(temp, temp + new Vector3(-1.78f, 1f, 0));
+            Gizmos.DrawLine(temp, temp + new Vector3(-1.78f, -1f, 0));
+            angler = Mathf.Atan2(temp.y - finalY, temp.x - finalX) * 180 / Mathf.PI;
+        }
+        else if (limbsCollisionChecker.rightFoot)
+        {
+            Gizmos.DrawLine(p3.position, p3.position + new Vector3(1f, 3.8f, 0));
+            Gizmos.DrawLine(p3.position, p3.position + new Vector3(-1f, 3.8f, 0));
+            angler = Mathf.Atan2(p3.position.y - finalY, p3.position.x - finalX) * 180 / Mathf.PI;
+        }
+        else if (limbsCollisionChecker.leftFoot && limbsCollisionChecker.leftHand)
+        {
+            Vector3 temp = new Vector3((p2.position.x + p4.position.x) / 2, (p2.position.y + p4.position.y) / 2, (p2.position.z + p4.position.z) / 2);
+            Gizmos.DrawLine(temp, temp + new Vector3(1.78f, 1f, 0));
+            Gizmos.DrawLine(temp, temp + new Vector3(1.78f, -1f, 0));
+            angler = Mathf.Atan2(temp.y - finalY, temp.x - finalX) * 180 / Mathf.PI;
+        }
+        else if (limbsCollisionChecker.leftFoot)
+        {
+            Gizmos.DrawLine(p4.position, p4.position + new Vector3(1f, 3.8f, 0));
+            Gizmos.DrawLine(p4.position, p4.position + new Vector3(-1f, 3.8f, 0));
+            angler = Mathf.Atan2(p4.position.y - finalY, p4.position.x - finalX) * 180 / Mathf.PI;
+        } 
+
+
+
+        Gizmos.color = Color.blue;
+        if(limbsCollisionChecker.rightHand && limbsCollisionChecker.leftHand)
+        {
+            Vector3 temp = new Vector3(x1, y1, z1);
+            Gizmos.DrawLine(temp, temp + new Vector3(1f, -.6f, 0));
+            Gizmos.DrawLine(temp, temp + new Vector3(-1f, -.6f, 0));
+        } else if (limbsCollisionChecker.rightHand)
+        {
+            Gizmos.DrawLine(p1.position, p1.position + new Vector3(1f, -1.78f, 0));
+            Gizmos.DrawLine(p1.position, p1.position + new Vector3(-1f, -1.78f, 0));
+        } else if (limbsCollisionChecker.leftHand)
+        {
+            Gizmos.DrawLine(p2.position, p2.position + new Vector3(1f, -1.78f, 0));
+            Gizmos.DrawLine(p2.position, p2.position + new Vector3(-1f, -1.78f, 0));
+        }
+
+         
+
+        Debug.Log("Leg angle: " + angler);
     }
 }
