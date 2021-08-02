8a375c9a1d2fc9b544377a7b9f26a28fc746d779
kmiecikJob <pracakmiecik@gmail.com>
2021-08-02 15:52:04 +0200

    Working on saving system

--- Assets/PersistableSO.cs
+++ Assets/PersistableSO.cs
@@ -5,53 +5,60 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 public class PersistableSO : MonoBehaviour
 {
-    [SerializeField] private Transform level;
+    public CustomWalllsCount wallsList;
+
     private void Start()
     {
-        LoadLevel();
+        LoadCustomWallsList();
     }
-
-    public void SaveNewLevel()
+    public void SaveNewLevel(string wallName, Transform level)
     {
-        SavePosition();
+        SaveWall(wallName, level);
     }
 
-    public List<Handle> LoadNewLevel()
+    public List<Handle> LoadNewLevel(string wallName)
     {
         
-        return LoadLevel();
+        return LoadWall(wallName);
     }
 
-    private void SavePosition()
+    public void SaveWall(string wallName, Transform level)
     {
+        wallsList.customWalls.Add(wallName);
+        BinaryFormatter bf2 = new BinaryFormatter();
+        FileStream file2 = File.Create(Application.persistentDataPath + string.Format("/{0}.pso", "CustomWallsList22190941"));
+        var json2 = JsonUtility.ToJson(wallsList);
+        bf2.Serialize(file2, json2);
+        file2.Close();
+
+
         BinaryFormatter bf = new BinaryFormatter();
         for(int i = 1; i < level.childCount; i++)
         {
-            FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}{1}.pso", "Hole", i));
+            FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}{1}.pso", wallName, i));
 
             Handle h = new Handle(level.GetChild(i).GetComponent<HandleDifficulty>().ScriptableHandle, level.GetChild(i).GetComponentInChildren<Renderer>().material.color,
                 level.GetChild(i).transform.position, level.GetChild(i).transform.localScale, level.GetChild(i).transform.rotation);
 
-
             var json = JsonUtility.ToJson(h);
             bf.Serialize(file, json);
             file.Close();
-            Debug.Log(h);
+            Debug.Log(Application.persistentDataPath);
         }
+        
     }
 
-    private List<Handle> LoadLevel()
+    private List<Handle> LoadWall(string wallName)
     {
-        Debug.Log(Application.persistentDataPath);
-        if (File.Exists(Application.persistentDataPath + string.Format("/{0}{1}.pso", "Hole", 1)))
+        if (File.Exists(Application.persistentDataPath + string.Format("/{0}{1}.pso", wallName, 0)))
         {
             List<Handle> handlesList = new List<Handle>();
-            for (int i = 1; i < 1000; i++)
+            for (int i = 0; i < 1000; i++)
             {
-                if (File.Exists(Application.persistentDataPath + string.Format("/{0}{1}.pso", "Hole", i)))
+                if (File.Exists(Application.persistentDataPath + string.Format("/{0}{1}.pso", wallName, i)))
                 {
                     BinaryFormatter bf = new BinaryFormatter();
-                    FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}{1}.pso", "Hole", i), FileMode.Open);
+                    FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}{1}.pso", wallName, i), FileMode.Open);
                     Handle h = JsonUtility.FromJson<Handle>((string)bf.Deserialize(file));
                     handlesList.Add(h);
                     file.Close();
@@ -65,5 +72,17 @@ public class PersistableSO : MonoBehaviour
             Debug.Log("File not found");
         }
         return null;
+    }
+
+    public void LoadCustomWallsList()
+    {
+        if (File.Exists(Application.persistentDataPath + string.Format("/{0}.pso", "CustomWallsList22190941"))){
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}.pso", "CustomWallsList22190941"), FileMode.Open);
+            CustomWalllsCount custom = JsonUtility.FromJson<CustomWalllsCount>((string)bf.Deserialize(file));
+            wallsList = custom;
+            file.Close();
+        }
+
     }
 }
--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -6,12 +6,21 @@ public class Wall : MonoBehaviour
 {
     public List<Handle> handlesList = new List<Handle>();
     [SerializeField] private PersistableSO persistableSO;
-
     private void Start()
     {
-        handlesList =  persistableSO.LoadNewLevel();
+        
+    }
 
-        foreach(Handle h in handlesList)
+    public void LoadLevel(string levelName)
+    {
+        handlesList = persistableSO.LoadNewLevel(levelName);
+
+        for(int i = 1; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i));
+        }
+
+        foreach (Handle h in handlesList)
         {
             GameObject temp = Instantiate(h.scriptableHandle.prefabToInstantiate, transform);
             temp.GetComponentInChildren<Renderer>().material.color = h.handleColor;
